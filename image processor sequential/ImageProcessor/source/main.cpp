460461082df33b77efa1a567c9e75c7bf738ab41
thuyet-mt14072508 <92238935+thuyet-mt14072508@users.noreply.github.com>
2023-02-12 13:14:46 +0700

    Create main.cpp

--- image processor sequential/ImageProcessor/source/main.cpp
+++ image processor sequential/ImageProcessor/source/main.cpp
@@ -1,17 +1,15 @@
- #include <iostream>
+#include <iostream>
 #include <vector>
 #include <chrono>
 #include <cmath>
 #include <cassert>
 #include <SFML/Graphics/Image.hpp>
+#include <omp.h>
 #include "FiltersProvider.hpp"
 
-const std::string IMAGE_NAME = "guitar";
+const std::string IMAGE_NAME = "test2";
 const std::string INPUT_IMAGE_NAME = IMAGE_NAME + ".jpg";
 const std::string OUTPUT_IMAGE_NAME = IMAGE_NAME + "_out.jpg";
-
-using KernelRow = std::vector<float>;
-using Kernel = std::vector<KernelRow>;
 
 template <typename Callable, typename... Args>
 auto runWithTimeMeasurementCpu(Callable&& function, Args&&... params)
@@ -41,7 +39,7 @@ void alignChannel(int& channelValue)
     channelValue = (channelValue < 0) ? 0 : channelValue;
 }
 
-void applyFilter(sf::Image& image, const Kernel& filter)
+void applyFilter(sf::Image& image, const Filter::Kernel& filter)
 {
     const auto kernelSize = static_cast<int>(filter.size());
     const auto kernelMargin = kernelSize / 2;
@@ -49,9 +47,12 @@ void applyFilter(sf::Image& image, const Kernel& filter)
     const auto imageWidth = static_cast<int>(image.getSize().x);
     auto outputImage = image;
 
-    for (int y = kernelMargin; y < imageHeight - kernelMargin; ++y)
+    int x, y;
+
+    #pragma omp parallel for private(x, y) schedule(dynamic)
+    for (y = kernelMargin; y < imageHeight - kernelMargin; ++y)
     {
-        for (int x = kernelMargin; x < imageWidth - kernelMargin; ++x)
+        for (x = kernelMargin; x < imageWidth - kernelMargin; ++x)
         {
             int newRedChannel{}, newGreenChannel{}, newBlueChannel{};
             for (int kernelX = -kernelMargin; kernelX <= kernelMargin; ++kernelX)
@@ -78,10 +79,39 @@ void applyFilter(sf::Image& image, const Kernel& filter)
 
 int main()
 {
+    omp_set_num_threads(omp_get_max_threads());
     auto image = loadImage();
-    const auto filter = Filter::blurKernel();
-    const auto duration = runWithTimeMeasurementCpu(applyFilter, image, filter);
-    std::cout << "Duration [ms]: " << duration << std::endl;
-    saveImage(image);
+
+    // Apply each of the 5 filters and save the output image
+    auto edgeDetectionFilter = Filter::edgeDetectionKernel();
+    auto edgeDetectionImage = image;
+    const auto edgeDetectionDuration = runWithTimeMeasurementCpu(applyFilter, edgeDetectionImage, edgeDetectionFilter);
+    std::cout << "Duration for Edge Detection OMP [ms]: " << edgeDetectionDuration << std::endl;
+    edgeDetectionImage.saveToFile("../images/" + IMAGE_NAME + "_edge_detection_omp.jpg");
+
+    auto blurFilter = Filter::blurKernel();
+    auto blurImage = image;
+    const auto blurDuration = runWithTimeMeasurementCpu(applyFilter, blurImage, blurFilter);
+    std::cout << "Duration for Blur OMP [ms]: " << blurDuration << std::endl;
+    blurImage.saveToFile("../images/" + IMAGE_NAME + "_blur_omp.jpg");
+
+    auto sharpenFilter = Filter::sharpenKernel();
+    auto sharpenImage = image;
+    const auto sharpenDuration = runWithTimeMeasurementCpu(applyFilter, sharpenImage, sharpenFilter);
+    std::cout << "Duration for Sharpen OMP [ms]: " << sharpenDuration << std::endl;
+    sharpenImage.saveToFile("../images/" + IMAGE_NAME + "_sharpen_omp.jpg");
+
+    auto embossFilter = Filter::embossKernel();
+    auto embossImage = image;
+    const auto embossDuration = runWithTimeMeasurementCpu(applyFilter, embossImage, embossFilter);
+    std::cout << "Duration for Emboss OMP [ms]: " << embossDuration << std::endl;
+    embossImage.saveToFile("../images/" + IMAGE_NAME + "_emboss_omp.jpg");
+
+    auto outlineFilter = Filter::outlineKernel();
+    auto outlineImage = image;
+    const auto boxBlurDuration = runWithTimeMeasurementCpu(applyFilter, outlineImage, outlineFilter);
+    std::cout << "Duration for Outline OMP [ms]: " << boxBlurDuration << std::endl;
+    outlineImage.saveToFile("../images/" + IMAGE_NAME + "_outline_omp.jpg");
+
     return EXIT_SUCCESS;
 }
